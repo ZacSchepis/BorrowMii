2143e7157a6c5ee8861c32a4c16f8f7c8714167d
Baylor <blueslugger48@gmail.com>
2023-03-21 20:54:50 -0600

    minor comment changes

--- lib/modelViewController.dart
+++ lib/modelViewController.dart
@@ -1,16 +1,11 @@
 class modelViewController {
   static final modelViewController _instance = modelViewController._internal();
-
-  // using a factory is important
-  // because it promises to return _an_ object of this type
-  // but it doesn't promise to make a new one.
+  //create instance
   factory modelViewController() {
     return _instance;
   }
 
-  // This named constructor is the "real" constructor
-  // It'll be called exactly once, by the static property assignment above
-  // it's also private, so it can only be called in this class
+ //constructor
   modelViewController._internal() {
     // initialization logic
     print("test mvc");
